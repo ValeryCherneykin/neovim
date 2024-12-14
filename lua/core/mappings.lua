6cc23b7910ad9936285db0e15b70cdc3a2c269fc
ValeryCherneykin <chernecin@icloud.com>
2024-12-14 16:23:54 +0300

    Add plugin neo-tree

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -7,6 +7,10 @@ vim.keymap.set('i','jj','<Esc>')
 -- Buffers
 vim.keymap.set('n','<leader>w',':w<CR>')
 
+-- Neo-tree
+vim.keymap.set('n','<leader>E',':Neotree left toggle reveal<CR>')
+vim.keymap.set('n','<leader>e',':Neotree float toggle reveal<CR>')
+
 -- Navigation
 vim.keymap.set('n','<C-k>',':wincmd k<CR>')
 vim.keymap.set('n','<C-j>',':wincmd j<CR>')
@@ -16,4 +20,3 @@ vim.keymap.set('n','<C-l>',':wincmd l<CR>')
 -- Splits
 vim.keymap.set('n','|',':vsplit<CR>')
 vim.keymap.set('n','\\',':split<CR>')
-
