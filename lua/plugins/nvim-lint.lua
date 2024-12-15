cbed6021e978e3f08f4fa234edfdd41c05cd8ae5
ValeryCherneykin <chernecin@icloud.com>
2024-12-15 22:51:02 +0300

    fixed lint for golang

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -5,12 +5,13 @@ return {
       require("lint").linters_by_ft = {
         markdown = { "vale" },
         cpp = { "clangtidy", "cpplint" },
-        go = { "golangci-lint" },
+        go = { "golangcilint" },
       }
+
+      -- Автозапуск линтера на сохранение файла
       vim.api.nvim_create_autocmd({ "BufWritePost" }, {
         callback = function()
           require("lint").try_lint()
-          require("lint").try_lint "cspell"
         end,
       })
     end,
