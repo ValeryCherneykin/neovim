1f79f2b49bbee708eaaa8b80f69c0beedcfbbf55
ValeryCherneykin <chernecin@icloud.com>
2024-12-15 12:33:41 +0300

    Add plugin mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,6 +4,15 @@ return {
 	config = function()
 		require('mason').setup{}
 	end
-	
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup(
+			{
+				ensure_installed = { "lua_ls", "rust_analyzer","gopls"},
+			}
+			)
+		end
 	}
 }
